765d9500b5981f970bd8f1fa47d2fa8f823e465d
Thomas Wiik <thomas.wiik@no.experis.com>
2024-08-27 12:36:32 +0200

    Finished extensions.

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/counter")
 public class CounterController {
     private int counter = 0;
+    private HashMap<String, Integer> counters= new HashMap<>();
 
     @GetMapping
     public int getCounter(){
@@ -23,5 +27,24 @@ public class CounterController {
     @GetMapping("/decrement")
     public int decrement(){
         return --this.counter;
+    }
+
+    @GetMapping("/custom/{name}")
+    public int getCustomCounter(@PathVariable String name){
+        if (counters.getOrDefault(name, -1).equals(-1))
+        {
+            counters.put(name, 0);
+        }
+        return counters.get(name);
+    }
+
+    @GetMapping("/custom/{name}/increment")
+    public int incrementCustomCounter(@PathVariable String name){
+        return counters.merge(name, 1, Integer::sum);
+    }
+
+    @GetMapping("/custom/{name}/decrement")
+    public int decrementCustomCounter(@PathVariable String name){
+        return counters.merge(name, -1, Integer::sum);
     }
 }
